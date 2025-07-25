68d7fe66cb44073febe57e1f80954c016eda4634
camille-s <camille.seaberry@gmail.com>
2025-07-25 11:23:52 -0400

    Add config file to tracking

---
+++ dist/theme/config.theme.js
@@ -0,0 +1,8 @@
+Nominatim_Config.Nominatim_API_Endpoint = 'http://localhost:8080/';
+Nominatim_Config.Page_Title = 'Nominatim geocoder';
+// Baltimore MD
+Nominatim_Config.Map_Default_Lat = 39.291;
+Nominatim_Config.Map_Default_Lon = -76.611;
+Nominatim_Config.Map_Default_Zoom = 12;
+Nominatim_Config.Map_Tile_URL = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
+Nominatim_Config.Map_Tile_Attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
