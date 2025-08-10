19117bfb09bb760764702f69947d549b206e4b90
camille-s <camille.seaberry@gmail.com>
2025-08-09 22:23:54 -0400

    Switch back to OSM tiles

--- dist/theme/config.theme.js
+++ dist/theme/config.theme.js
@@ -4,6 +4,6 @@ Nominatim_Config.Page_Title = 'Nominatim geocoder';
 Nominatim_Config.Map_Default_Lat = 39.291;
 Nominatim_Config.Map_Default_Lon = -76.611;
 Nominatim_Config.Map_Default_Zoom = 12;
-Nominatim_Config.Map_Tile_URL = 'https://{s}.tile.openstreetmap.fr/hot/{z}/{x}/{y}.png';
-Nominatim_Config.Map_Tile_Attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Tiles style by <a href="https://www.hotosm.org/" target="_blank">Humanitarian OpenStreetMap Team</a> hosted by <a href="https://openstreetmap.fr/" target="_blank">OpenStreetMap France</a>'
+Nominatim_Config.Map_Tile_URL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
+Nominatim_Config.Map_Tile_Attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 Nominatim_Config.Search_AreaPolygons = false;
