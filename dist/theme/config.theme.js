407862357da36b1f795bf641816d8ccf67b2e319
camille-s <camille.seaberry@gmail.com>
2025-08-09 22:28:06 -0400

    Change default zoom

--- dist/theme/config.theme.js
+++ dist/theme/config.theme.js
@@ -3,7 +3,7 @@ Nominatim_Config.Page_Title = 'Nominatim geocoder';
 // Baltimore MD
 Nominatim_Config.Map_Default_Lat = 39.291;
 Nominatim_Config.Map_Default_Lon = -76.611;
-Nominatim_Config.Map_Default_Zoom = 12;
+Nominatim_Config.Map_Default_Zoom = 15;
 Nominatim_Config.Map_Tile_URL = 'https://tile.openstreetmap.org/{z}/{x}/{y}.png';
 Nominatim_Config.Map_Tile_Attribution = '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 Nominatim_Config.Search_AreaPolygons = false;
