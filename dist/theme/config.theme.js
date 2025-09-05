dbb03537220c3caeefae082b512ad78b7eacc275
camille-s <camille.seaberry@gmail.com>
2025-09-05 11:02:15 -0400

    Go back to hardcoded hostname, port for server

--- dist/theme/config.theme.js
+++ dist/theme/config.theme.js
@@ -1,9 +1,12 @@
-const host = process.env.SERVER_HOST || 'geocoder';
-const port = process.env.SERVER_PORT || '8123';
-// Nominatim_Config.Nominatim_API_Endpoint = 'http://localhost:8123/';
+// import { SERVER_HOST, SERVER_PORT } from '../custom-env.js';
+// Nominatim_Config.Page_Title = 'Nominatim geocoder';
+// const host = SERVER_HOST || 'geocoder';
+// const port = SERVER_PORT || '8123';
+// go back to hardcoding for now
+const host = 'geocoder';
+const port = '8123';
 Nominatim_Config.Nominatim_API_Endpoint = `http://${host}:${port}/`;
-Nominatim_Config.Page_Title = 'Nominatim geocoder';
-// Baltimore MD
+// coords for Baltimore, MD
 Nominatim_Config.Map_Default_Lat = 39.291;
 Nominatim_Config.Map_Default_Lon = -76.611;
 Nominatim_Config.Map_Default_Zoom = 15;
